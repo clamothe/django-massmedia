9f5d9cc4329d8aa294090c05887d60a5e257f44d
Charlie La Mothe <charlie@clamothe.com>
2009-02-19 12:22:42 -0800

    massmedia.views: * replaces if/else tree in widget and list_by_type with a dictionary lookup. * replaces 'id' function arguments with 'pk' to avoid overriding the built-in id function.
    massmedia.urls:
    * id replaced with pk to reflect changes in massmedia.views.

--- massmedia/urls.py
+++ massmedia/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('massmedia.views',
     (r'type/(?P<type>[-\w]+)/$', 'list_by_type'),
-    (r'collection/(?P<id>\d+)/(?P<type>[-\w]+)/$', 'list_by_collection_by_type'),
-    (r'collection/(?P<id>\d+)/$', 'list_by_collection'),
-    (r'widget/(?P<id>\d+)/(?P<type>[-\w]+)/$', 'widget'),
+    (r'collection/(?P<pk>\d+)/(?P<type>[-\w]+)/$', 'list_by_collection_by_type'),
+    (r'collection/(?P<pk>\d+)/$', 'list_by_collection'),
+    (r'widget/(?P<pk>\d+)/(?P<type>[-\w]+)/$', 'widget'),
     ('', 'list'),
 )
--- massmedia/views.py
+++ massmedia/views.py
@@ -5,26 +5,34 @@ from django.conf import settings
 from django.http import Http404, HttpResponse
 from django.contrib.contenttypes.models import ContentType
 
-def widget(request, id, media_type):
-    if media_type == 'image':  model = Image
-    elif media_type == 'audio':  model = Audio
-    elif media_type == 'flash':  model = Flash
-    elif media_type == 'video':  model = Video
-    else: return HttpResponse('%s not found' % media_type)
+# str -> Model
+media_types = {
+    'image': Image,
+    'audio': Audio,
+    'flash': Flash,
+    'video': Video,
+}
+
+def widget(request, media_pk, media_type):
+    try:
+        model = media_types[media_type]
+    except KeyError:
+        return HttpResponse('%s not found' % media_type)
+    
     try:
         return render_to_response('massmedia/inline.html', {
-            'media':model.objects.get(pk=id),
-            'type':media_type
+            'media': model.objects.get(pk=media_pk),
+            'type': media_type
         })
     except model.DoesNotExist:
-        return HttpResponse('%s #%s not found' % (media_type, id))
+        return HttpResponse('%s #%s not found' % (media_type, media_pk))
 
 def list_by_type(request, media_type):
-    if media_type == 'image':  model = Image
-    elif media_type == 'audio':  model = Audio
-    elif media_type == 'flash':  model = Flash
-    elif media_type == 'video':  model = Video
-    else: raise Http404
+    try:
+        model = media_types[media_type]
+    except KeyError:
+        return HttpResponse('%s not found' % media_type)
+
     return render_to_response('massmedia/list.html', {
         'objects': model.objects.filter(
             public=True,
@@ -32,19 +40,19 @@ def list_by_type(request, media_type):
         )
     })
 
-def list_by_collection(request, id):
+def list_by_collection(request, pk):
     return render_to_response('massmedia/list.html', {
         'objects': [x.content_object for x in CollectionRelation.objects.filter(
-            collection=get_object_or_404(Collection, pk=id),
+            collection=get_object_or_404(Collection, pk=pk),
             collection__public=True,
             collection__sites__id__exact=settings.SITE_ID
         )]
     })
 
-def list_by_collection_by_type(request, id, media_type):
+def list_by_collection_by_type(request, pk, media_type):
     return render_to_response('massmedia/list.html', {
         'objects': [x.content_object for x in CollectionRelation.objects.filter(
-            collection=get_object_or_404(Collection, pk=id),
+            collection=get_object_or_404(Collection, pk=pk),
             collection__public=True,
             collection__sites__id__exact=settings.SITE_ID,
             content_type=ContentType.objects.get(name=media_type),
