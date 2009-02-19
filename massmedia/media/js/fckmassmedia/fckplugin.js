d9e2b3c63aea8e5b9c4d15fb61b6e104eccc34f9
jquick <jquick@32bf7fcd-2366-4af3-8dd0-2a66dc460191>
2009-02-19 15:20:54 +0000

    audio?

--- massmedia/media/js/fckmassmedia/fckplugin.js
+++ massmedia/media/js/fckmassmedia/fckplugin.js
@@ -5,7 +5,7 @@ MassMediaCommand.GetState=function() {
     return FCK_TRISTATE_OFF; //we dont want the button to be toggled
 }
 MassMediaCommand.Execute=function() {
-    window.open('/admin/massmedia/flash/?t=id&pop=1', 'insertVariable', 'width=500,height=400,scrollbars=no,scrolling=no,location=no,toolbar=no');
+    window.open('/admin/massmedia/audio/?t=id&pop=1', 'insertVariable', 'width=700,height=400,scrollbars=yes,scrolling=yes,location=no,toolbar=no');
 }
 FCKCommands.RegisterCommand('MassMedia', MassMediaCommand ); //otherwise our command will not be found
 var oMassMedias = new FCKToolbarButton('MassMedia', 'insert media');
