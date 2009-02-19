06aef3e06fce2b2840b81a166471337405b48f3f
Charlie La Mothe <charlie@clamothe.com>
2009-02-19 11:52:45 -0800

    type changed to media_type in views, to avoid conflict with builtin type function.

--- massmedia/views.py
+++ massmedia/views.py
@@ -5,25 +5,25 @@ from django.conf import settings
 from django.http import Http404, HttpResponse
 from django.contrib.contenttypes.models import ContentType
 
-def widget(request, id, type):
-    if type == 'image':  model = Image
-    elif type == 'audio':  model = Audio
-    elif type == 'flash':  model = Flash
-    elif type == 'video':  model = Video
-    else: return HttpResponse('%s not found'%type)
+def widget(request, id, media_type):
+    if media_type == 'image':  model = Image
+    elif media_type == 'audio':  model = Audio
+    elif media_type == 'flash':  model = Flash
+    elif media_type == 'video':  model = Video
+    else: return HttpResponse('%s not found'%media_type)
     try:
         return render_to_response('massmedia/inline.html', {
             'media':model.objects.get(pk=id),
-            'type':type
+            'type':media_type
         })
     except model.DoesNotExist:
-        return HttpResponse('%s #%s not found'%(type, id))
+        return HttpResponse('%s #%s not found'%(media_type, id))
 
-def list_by_type(request, type):
-    if type == 'image':  model = Image
-    elif type == 'audio':  model = Audio
-    elif type == 'flash':  model = Flash
-    elif type == 'video':  model = Video
+def list_by_type(request, media_type):
+    if media_type == 'image':  model = Image
+    elif media_type == 'audio':  model = Audio
+    elif media_type == 'flash':  model = Flash
+    elif media_type == 'video':  model = Video
     else: raise Http404
     return render_to_response('massmedia/list.html', {
         'objects': model.objects.filter(
@@ -41,13 +41,13 @@ def list_by_collection(request, id):
         )]
     })
 
-def list_by_collection_by_type(request, id, type):
+def list_by_collection_by_type(request, id, media_type):
     return render_to_response('massmedia/list.html', {
         'objects': [x.content_object for x in CollectionRelation.objects.filter(
             collection=get_object_or_404(Collection, pk=id),
             collection__public=True,
             collection__sites__id__exact=settings.SITE_ID,
-            content_type=ContentType.objects.get(name=type),
+            content_type=ContentType.objects.get(name=media_type),
         )]
     })
     
