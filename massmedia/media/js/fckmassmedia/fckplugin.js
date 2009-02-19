40b4eaa3ccd17e3481ce3098ec07bce70f44c7ec
jquick <jquick@32bf7fcd-2366-4af3-8dd0-2a66dc460191>
2009-02-19 15:03:38 +0000

    start of popup plugin

--- massmedia/media/js/fckmassmedia/fckplugin.js
+++ massmedia/media/js/fckmassmedia/fckplugin.js
@@ -1,13 +1,13 @@
-var InsertVariableCommand=function(){
+var MassMediaCommand=function(){
     //create our own command, we dont want to use the FCKDialogCommand because it uses the default fck layout and not our own
 };
-InsertVariableCommand.GetState=function() {
+MassMediaCommand.GetState=function() {
     return FCK_TRISTATE_OFF; //we dont want the button to be toggled
 }
-InsertVariableCommand.Execute=function() {
+MassMediaCommand.Execute=function() {
     window.open('insertVariable.do', 'insertVariable', 'width=500,height=400,scrollbars=no,scrolling=no,location=no,toolbar=no');
 }
-FCKCommands.RegisterCommand('Insert_Variables', InsertVariableCommand ); //otherwise our command will not be found
-var oInsertVariables = new FCKToolbarButton('Insert_Variables', 'insert variable');
-oInsertVariables.IconPath = FCKConfig.PluginsPath + 'insertvariables/variable.gif'; //specifies the image used in the toolbar
-FCKToolbarItems.RegisterItem( 'Insert_Variables', oInsertVariables );
+FCKCommands.RegisterCommand('MassMedia', MassMediaCommand ); //otherwise our command will not be found
+var oMassMedias = new FCKToolbarButton('MassMedia', 'insert media');
+oMassMedias.IconPath = FCKConfig.PluginsPath + 'icon.png'; //specifies the image used in the toolbar
+FCKToolbarItems.RegisterItem( 'MassMedia', oMassMedias );
